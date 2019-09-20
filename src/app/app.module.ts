3e59e575fa856b49f38f4558dbfbf2c09e50992b
riyakuruvilla <riya.kuruvilla@experionglobal.com>
2019-09-20 10:24:23 +0530

    listingproduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    ProductComponent,
+   
   ],
   imports: [
     BrowserModule
